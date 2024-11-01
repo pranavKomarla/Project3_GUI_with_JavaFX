56f2d9ab25beb6722c8ee6f5ec71f90f4d46927f
Pranav Komarla <pranavkv03@gmail.com>
2024-11-01 02:57:11 -0400

    finishing first tab

--- src/main/java/clinic/ClinicManagerController.java
+++ src/main/java/clinic/ClinicManagerController.java
@@ -4,11 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class ClinicManagerController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        //welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
